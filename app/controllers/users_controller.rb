6faa503e39df1c73484b9d9a091204dd57edc007
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-08-06 17:17:37 +0200

    implemented update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,37 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
 
   def new 
     @user = User.new
   end
+
+  def edit
+  end
   
-  def create
-    @user = User.new(users_params)
-    if @user.save
-      flash[:notice] = "Welcome #{@user.username}! You have successfully signed up!"
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "You have updated the account successfully!"
       redirect_to articles_path
     else
       render 'new'
     end
   end
 
+  def create
+    @user = User.new(users_params)
+    if @user.save
+      flash[:notice] = "Welcome #{@user.username}! You have successfully signed up!"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def users_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end

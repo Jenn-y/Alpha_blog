0f7b4610c15f73c084a32e86d85783ea874e5dcc
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-08-06 17:05:35 +0200

    implemented create action for user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,16 @@ class UsersController < ApplicationController
   end
   
   def create
+    @user = User.new(users_params)
+    if @user.save
+      flash[:notice] = "Welcome #{@user.username}! You have successfully signed up!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def users_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end

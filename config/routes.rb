2f275c1a0529303b18968da08733d4c2a1fc1521
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-08-06 15:48:20 +0200

    added signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles
-  resources :users
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   get 'about', to: 'pages#about'
   root 'pages#home'
 end

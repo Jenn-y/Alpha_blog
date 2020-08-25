ff6dfc9aef57c64dd79a07e352dffcabd7bb7d92
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-08-25 11:53:32 +0200

    added test folder, created category model and initial test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_24_150448) do
+ActiveRecord::Schema.define(version: 2020_08_25_095011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2020_08_24_150448) do
     t.datetime "updated_at"
     t.text "description"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|

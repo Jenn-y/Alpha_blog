3c791acbb58699b5808ae3ed5b02f7bee87699a6
Dzenita Djulovic <djulovic.dzenita1999@gmail.com>
2020-08-26 14:53:54 +0200

    implemented many-to-many association between articles and categories, finalized styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
 
   validates_presence_of :title, :description
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
 end
